7c338ac566b8ae6b3a8f812b20edcb341eeb5f3d
Obinna Akpulonu <akpulonujoseph@gmail.com>
2024-06-11 08:22:29 +0100

    Update urls.py

--- test_project/urls.py
+++ test_project/urls.py
@@ -2,12 +2,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from accounts.views import home, signup_view, login_view, logout_view
+from accounts.views import home, signup_view, login_view, logout_view, create_superuser_view
 
 urlpatterns = [
     path('', home, name='home'),
     path('signup/', signup_view, name='signup'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
+    path('create-superuser/', create_superuser_view, name='create_superuser'),
     path('admin/', admin.site.urls),
 ]
